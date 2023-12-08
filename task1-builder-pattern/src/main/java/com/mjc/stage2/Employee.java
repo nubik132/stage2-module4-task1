2e624b319a6eff3e01febc3f60d78537fed26111
nubik132 <mister.nubik@list.ru>
2023-12-08 19:14:42 +0300

    complete task

--- task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
+++ task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
@@ -69,6 +69,35 @@ public class Employee {
     }
 
     public static class EmployeeBuilder {
-        // Write your code here!
+       private Employee employee;
+
+        public EmployeeBuilder() {
+            this.employee = new Employee();
+        }
+
+        public EmployeeBuilder setLastName(String lastName){
+            employee.setLastName(lastName);
+            return this;
+        }
+        public EmployeeBuilder setPosition(String position){
+            employee.setPosition(position);
+            return this;
+        }
+        public EmployeeBuilder setPhone(String phone){
+            employee.setPhone(phone);
+            return this;
+        }
+        public EmployeeBuilder setEmail(String email){
+            employee.setEmail(email);
+            return this;
+        }
+        public EmployeeBuilder setCarNumber(String carNumber){
+            employee.setCarNumber(carNumber);
+            return this;
+        }
+        public Employee build(){
+            return employee;
+        }
+
     }
 }
--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,16 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    // Write your code here!
+    private static ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton() {}
+
+    synchronized public static ThreadSafeSingleton getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new ThreadSafeSingleton();
+        }
+        return instance;
+    }
 }
